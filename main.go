f4be9bddfeecd0d5530673295850fff8385c34af
Lars Larsson <lars.la@gmail.com>
2019-08-17 15:43:23 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"sort"
+	"fmt"
+	"strings"
+	"log"
+	"os"
+	"bufio"
+)
+
+func main() {
+	all := make(map[string][]string)
+
+    file, err := os.Open("words.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+		word := scanner.Text()
+		sorted := sortChars(word)
+		if _, ok := all[sorted]; !ok {
+			all[sorted] = make([]string, 0)
+		}
+		all[sorted] = append(all[sorted], word)
+    }
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+	}
+	
+	highest := struct {
+		word string
+		count int
+	}{
+		word: "",
+		count: 0,
+	}
+	for word, instances := range all {
+		if len(instances) > highest.count {
+			highest.count = len(instances)
+			highest.word = word
+		}
+	}
+
+	fmt.Println("Winner: ", highest.word, highest.count)
+	for _, w := range all[highest.word] {
+		fmt.Println("  ", w)
+	}
+}
+
+func sortChars(word string) string {
+	sorted := strings.Split(word, "")
+	sort.Strings(sorted)
+
+	return strings.Join(sorted, "")
+}
